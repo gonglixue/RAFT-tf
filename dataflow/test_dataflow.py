858082a9dd5bb7b4b6a45fd6f74ea44b645567dd
Gong Lixue <724351282@qq.com>
2020-11-24 18:39:45 +0800

    fix test-dataflow augmentation

--- dataflow/test_dataflow.py
+++ dataflow/test_dataflow.py
@@ -63,8 +63,9 @@ class FlowDataProcess(object):
 
 
         # random switch order
-        if np.random.choice([0, 1]) > 0:
-            x[0], x[1] = x[1], x[0]
+        if self.aug_gen is not None:
+            if np.random.choice([0, 1]) > 0:
+                x[0], x[1] = x[1], x[0]
 
         height, width = x[0].shape[0: 2]
         if self.aug_rgb is not None: # TODO check input value range
